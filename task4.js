57cafb31ab8f4fd3bd440aa98f09b1b82e5c46a1
Satrio Kamaludin <satriokamaludin@gmail.com>
2020-10-18 13:58:56 +0700

    master

--- task4.js
+++ task4.js
@@ -15,18 +15,40 @@ class student {
     }
 
     setName(Nm) {
-        this.name = Nm;
+        if (isNaN(Nm)) {
+            this.name = Nm; 
+        } else {
+            throw Error('Must be alphabetical');
+        }
     }
 
     setAge(howold) {
-        this.age = howold;
+        if (Number.isInteger(howold)) {
+            this.age = howold;
+        }
+        else {
+            throw Error('Must be numbers');
+        }
     }
 
     setDoB(date) {
         this.dob = date;
     }
 
-    //need setGender function here; currently I have no I idea it's javascript implementation for the time being
+
+    // I don't even know how, I don't even know why, but for some reason conditional if-else-if doesn't worked on me, it's likely that I overlooked some misscodes which IDK where those are 
+    setGender(newGender) {  //I had to use Reno's linecode for this one, so credits to him for this one
+        switch (newGender.toLowerCase()) {
+            case "male":
+              this.gender = newGender;
+              break;
+            case "female":
+              this.gender = newGender;
+              break;
+            default:
+              throw Error('Must be male or female');  //Addition of error throwing if it's neither male or female, by yours truly
+        }
+    }
 
     addHobby(hobby) {
         return this.hobbies.push(hobby);
@@ -36,7 +58,15 @@ class student {
         return this.hobbies.pop(hobby);
     }
 
-    //need getData function here; I haven't been able to know how exactly this implementation works
+    get GetData() {  //this was originally belonged to Reno but with my breakline alterations, once again props to Reno for his original linecode.
+        return `
+        ID: ${this.id} 
+        Name: ${this.name}
+        Birthdate:  ${this.dob} 
+        Age: ${this.age}
+        Gender: ${this.gender}
+        Hobbies: ${this.hobbies}`;
+    }
 
 }
 
@@ -50,6 +80,8 @@ const student1 = new student ({
     hobbies: ['Building PC','Gaming','Watching VTuber']  //doesn't work, had to be done manually via student1.addHobby('blahblahblah')
 });
 
+
+
 const student2 = new student ({
     name: 'Edvia',
     id: '002',
@@ -58,3 +90,4 @@ const student2 = new student ({
     age: 23,
     hobbies: ['Vtubing','Gaming','Watching Anime']  //same goddamn problem but instead it's student2   
 });
+
